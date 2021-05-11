f16fea128dde815a2d7535b7434c2c19726ffa7d
FarhaD <80015595+FarhaD11011@users.noreply.github.com>
2021-05-11 01:01:55 -0500

    Add files via upload

---
+++ let-const-Exercise.js
@@ -0,0 +1,22 @@
+// ES2015 Global Constants
+const PI = 3.14;
+PI = 42; // Error!
+
+// Quiz
+What is the difference between var and let?
+// (var is function scoped and let is block scoped. 
+// It can be said that a variable declared with var is).
+
+What is the difference between var and const?
+// (var variables can be updated and re-declared within its scope;
+// const variables can neither be updated nor re-declared.
+//  They are all hoisted to the top of their scope.)
+
+What is the difference between let and const?
+// (let variables can be updated but not re-declared; 
+// const variables can neither be updated nor re-declared)
+
+What is hoisting?
+// (this means that no matter where functions and variables are declared,
+//  they are moved to the top of their scope 
+//  regardless of whether their scope is global or local)
